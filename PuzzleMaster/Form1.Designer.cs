256b6e84d23d13d76756684c7eb87dcf1054b820
Iva <60818270+iva7777@users.noreply.github.com>
2023-04-23 12:58:28 +0300

    puzzle form done
    to-do: make a start game form, add custom tab icon

---
+++ PuzzleMaster/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿
+namespace PuzzleMaster
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(3, 535);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 32);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ListOne";
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(660, 529);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(211, 38);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Status";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(871, 36);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            this.menuStrip1.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.menuStrip1_ItemClicked);
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(46, 24);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.OpenFileEvent);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(83)))), ((int)(((byte)(147)))), ((int)(((byte)(235)))));
+            this.ClientSize = new System.Drawing.Size(871, 630);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Puzzle Master";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+    }
+}
+
---
+++ PuzzleMaster/Form1.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PuzzleMaster
+{
+    public partial class Form1 : Form
+    {
+        List<PictureBox> pictureBoxList = new List<PictureBox>();
+        List<Bitmap> images = new List<Bitmap>();
+        List<string> locations = new List<string>();
+        List<string> currentLocations = new List<string>();
+
+        private string winPosition;
+        private string currentPosition;
+
+        private Bitmap mainBitmap;
+
+        public Form1()
+        {
+            InitializeComponent();
+            menuStrip1.BackColor = Color.FromArgb(122, 173, 243);
+        }
+
+        private void OpenFileEvent(object sender, EventArgs e)
+        {
+            if (pictureBoxList != null)
+            {
+                foreach (PictureBox picture in pictureBoxList.ToList())
+                {
+                    this.Controls.Remove(picture);
+                }
+                pictureBoxList.Clear();
+                images.Clear();
+                locations.Clear();
+                currentLocations.Clear();
+                winPosition = string.Empty;
+                currentPosition = string.Empty;
+                label2.Text = string.Empty;
+            }
+            OpenFileDialog open = new OpenFileDialog();
+            open.Filter = "Image Files Only | *.jpg; *.jpeg; *.gif; *.png;";
+            if (open.ShowDialog() == DialogResult.OK)
+            {
+                mainBitmap = new Bitmap(open.FileName);
+                CreatePictureBoxes();
+                AddImages();
+            }
+        }
+
+        private void CreatePictureBoxes()
+        {
+            for (int i = 0; i < 9; i++)
+            {
+                PictureBox tempPic = new PictureBox();
+                tempPic.Size = new Size(130, 130);
+                tempPic.Tag = i.ToString();
+                tempPic.Click += OnPicClick;
+                pictureBoxList.Add(tempPic);
+                locations.Add(tempPic.Tag.ToString());
+            }
+        }
+
+        private void OnPicClick(object sender, EventArgs e)
+        {
+            PictureBox pictureBox = (PictureBox)sender;
+            PictureBox emptyBox = pictureBoxList.Find(x => x.Tag.ToString() == "0");
+
+            Point firstPic = new Point(pictureBox.Location.X, pictureBox.Location.Y);
+            Point secondPic = new Point(emptyBox.Location.X, emptyBox.Location.Y);
+
+            var firstIndex = this.Controls.IndexOf(pictureBox);
+            var secondIndex = this.Controls.IndexOf(emptyBox);
+
+            if (pictureBox.Right == emptyBox.Left && pictureBox.Location.Y == emptyBox.Location.Y
+                || pictureBox.Left == emptyBox.Right && pictureBox.Location.Y == emptyBox.Location.Y
+                || pictureBox.Top == emptyBox.Bottom && pictureBox.Location.X == emptyBox.Location.X
+                || pictureBox.Bottom == emptyBox.Top && pictureBox.Location.X == emptyBox.Location.X)
+            {
+                pictureBox.Location = secondPic;
+                emptyBox.Location = firstPic;
+
+                this.Controls.SetChildIndex(pictureBox, secondIndex);
+                this.Controls.SetChildIndex(emptyBox, firstIndex);
+            }
+
+            label2.Text = "";
+            currentLocations.Clear();
+            CheckGame();
+        }
+
+        private void CropImage(Bitmap mainBitmap, int height, int width)
+        {
+            int x = 0;
+            int y = 0;
+
+            for (int blocks = 0; blocks < 9; blocks++)
+            {
+                Bitmap croppedImage = new Bitmap(height, width);
+                for (int i = 0; i < height; i++)
+                {
+                    for (int j = 0; j < width; j++)
+                    {
+                        croppedImage.SetPixel(i, j, mainBitmap.GetPixel((i + x), (j + y)));
+                    }
+                }
+
+                images.Add(croppedImage);
+                x += 130;
+                if (x == 390)
+                {
+                    x = 0;
+                    y += 130;
+                }
+            }
+        }
+
+        private void AddImages()
+        {
+            Bitmap tempBitmap = new Bitmap(mainBitmap, new Size(390, 390));
+            CropImage(tempBitmap, 130, 130);
+
+            for (int i = 1; i < pictureBoxList.Count; i++)
+            {
+                pictureBoxList[i].BackgroundImage = (Image)images[i];
+            }
+
+            PlacePictureBoxesToForm();
+        }
+
+        private void PlacePictureBoxesToForm()
+        {
+            //shuffle the parts of the puzzle
+            var shuffleImages = pictureBoxList.OrderBy(a => Guid.NewGuid()).ToList();
+            pictureBoxList = shuffleImages;
+
+            int x = 200;
+            int y = 25;
+
+            for (int i = 0; i < pictureBoxList.Count; i++)
+            {
+                pictureBoxList[i].BackColor = Color.Gold;
+
+                if (i == 3 || i == 6)
+                {
+                    y += 130;
+                    x = 200;
+                }
+
+                pictureBoxList[i].BorderStyle = BorderStyle.FixedSingle;
+                pictureBoxList[i].Location = new Point(x, y);
+
+                this.Controls.Add(pictureBoxList[i]);
+                x += 130;
+                winPosition += locations[i];
+            }
+        }
+
+        private void CheckGame()
+        {
+            foreach (Control x in this.Controls)
+            {
+                if (x is PictureBox)
+                {
+                    currentLocations.Add(x.Tag.ToString());
+                }
+            }
+            currentPosition = string.Join("", currentLocations);
+            label1.Text = winPosition;
+            label2.Text = currentPosition;
+
+            if (winPosition == currentPosition)
+            {
+                label2.Text = "Matched!";
+            }
+        }
+
+        private void menuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+            
+        }
+    }
+}
